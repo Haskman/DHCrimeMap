d0a8041b184d90313f2bf2a65540ddcb5ee9d907
Haskman <sahinmehmetutku@gmail.com>
2016-04-10 00:19:16 -0400

    Submit-Ready
    Working, submission-ready version

--- process.py
+++ process.py
@@ -8,11 +8,12 @@ Created on Sat Apr 09 17:52:49 2016
 import pandas as pd
 import json
 
-data_address = "http://data.louisvilleky.gov/sites/default/files/temp_import/LMPD_OP_BIAS.csv"
+data_address = "data/LMPD_OP_BIAS.csv"
 data = pd.read_csv(data_address)
 
 
 output = data.to_json(orient = "records")
+print output
 
 with open('data/Processed_Crime_Data.json', 'w') as outfile:
     json.dump(output, outfile)
